3f7b25f0d88d84c2924f929a7f9ef2c895574517
ghulammustafa119 <ghulammustafabhutto77@gmail.com>
2025-02-04 13:15:55 +0500

    commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,9 +7,9 @@ const Header = () => {
     <>
       <header className="fixed z-10 top-0 w-full text-gray-600">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          <Link href="/" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
             <span className="ml-3 text-xl font-bold">GMBHUTTO.</span>
-          </a>
+          </Link>
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
             <Link href="/" className="mr-5 hover:text-gray-900">Home</Link>
             <Link href="/about" className="mr-5 hover:text-gray-900">About</Link>
